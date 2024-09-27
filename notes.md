88f89bc2356d6224f15366f1c4a8b142c3b625a5
Omar Atya <omaratyamahboub@gmail.com>
2024-09-27 17:26:28 +0300

    js1

---
+++ notes.md
@@ -0,0 +1,11 @@
+#GIT
+
+###git status
+###git pull
+###git add . -> 
+###git commit -m ""
+###git push ->
+
+
+
+development -> staging -> production 
---
+++ scripts/index.js
@@ -0,0 +1,55 @@
+// let firstName = `ahmed`;
+// let testNumber = 25;
+// let testBoolean = true;
+// let testUndefined;
+// let testNull = null;
+// let testBigInt;
+// console.log(`Hello, ${firstName} and ${testNumber} and ${testBoolean} and ${testUndefined} and ${testNull}`);
+
+// let fname;
+// console.log(fname);
+// fname = "omar";
+// console.log(fname);
+// fname = 23;
+// console.log(fname);
+// fname = true;
+// console.log(fname);
+
+
+// let x = "26";
+// let y = 13;
+
+// console.log(x + y);
+// x = 26;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x % y);
+// console.log(x / y);
+// console.log(x ** y);
+
+//Mark's BMI calculation
+let markHeight = 1.93;
+let markMass = 78;
+let markBmi = markMass / markHeight ** 2;
+console.log("Mark's BMI = " + markBmi);
+
+//John's BMI calculation
+let johnHeight = 1.87;
+let johnMass = 60;
+let johnBmi = johnMass / johnHeight ** 2;
+console.log("John's BMI = " + johnBmi);
+
+//output
+console.log(`Mark Weighs ${markMass}kg and is ${markHeight}m tall.
+John weighs ${johnMass}kg and is ${johnHeight}m tall.`);
+
+
+
+//comparison output
+(markBmi > johnBmi) ? (console.log("Mark's BMI is bigger")) : (console.log("John's BMI is bigger"));
+
+
+
+
+//LOGICAL AND, OR
+
